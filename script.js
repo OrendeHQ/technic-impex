8d9f07bccd972bc4df11175877c955a3d78dcb06
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-01-03 21:36:25 +0800

    Fix small bugs

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ var $allNavLinks = $('.navbar-nav .nav-link');
 
 $(document).ready(function() {
   $(document).on('scroll', handleScroll);
-  
+
   $(document).on('click', '.scroll', function(e) {
     e.preventDefault();
     smoothScroll($(e.currentTarget).attr('href'), 50);
   });
-  
+
   $(document).on('submit', '.form-bordered', handleFormSubmit);
-  
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {    
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
     var target = $(this).attr('href');
     $('a[data-toggle="tab"]').removeClass('active');
     $('a[href="' + target + '"]').addClass('active');
@@ -20,19 +20,31 @@ $(document).ready(function() {
 function handleScroll() {
   var position = $(window).scrollTop();
   $allNavLinks.removeClass('active');
-  if (position >= $('#services').position().top && position < $('#products').position().top) {
+  if (
+    position >= $('#services').position().top &&
+    position < $('#products').position().top
+  ) {
     $('.nav-link[href="#services"]').addClass('active');
     var relativePos = position - $('#services').position().top;
-    if (relativePos >= $('#exporter').position().top && relativePos < $('#projects').position().top) {
+    if (
+      relativePos >= $('#exporter').position().top &&
+      relativePos < $('#projects').position().top
+    ) {
       $('#services').css('background-color', '#ffccac');
     } else if (relativePos >= $('#projects').position().top) {
       $('#services').css('background-color', '#ffeb94');
     } else {
       $('#services').css('background-color', '#c1d1dc');
     }
-  } else if (position >= $('#products').position().top && position < $('#about').position().top) {
+  } else if (
+    position >= $('#products').position().top &&
+    position < $('#about').position().top
+  ) {
     $('.nav-link[href="#products"]').addClass('active');
-  } else if (position >= $('#about').position().top && position < $('#contact').position().top) {
+  } else if (
+    position >= $('#about').position().top &&
+    position < $('#contact').position().top
+  ) {
     $('.nav-link[href="#about"]').addClass('active');
   } else if (position >= $('#contact').position().top) {
     $('.nav-link[href="#contact"]').addClass('active');
@@ -40,7 +52,10 @@ function handleScroll() {
 }
 
 function smoothScroll(anchor, offset) {
-  $('body').animate({scrollTop: $(anchor).offset().top - offset }, 'slow');
+  $('html,body').animate(
+    { scrollTop: $(anchor).offset().top - offset },
+    'slow',
+  );
 }
 
 function handleFormSubmit(e) {
@@ -53,13 +68,15 @@ function handleFormSubmit(e) {
   $.ajax({
     url: 'https://technicimpex.herokuapp.com',
     method: 'POST',
-    data: { name: name, email: email, message: message }
-  }).done(function (res) {
-    $form[0].reset();
-    $form.removeClass('sending');
-    alert('Thank you! We\'ve gotten your message!');
-  }).fail(function (res) {
-    alert(res.message);
-    $form.removeClass('sending');
-  });
+    data: { name: name, email: email, message: message },
+  })
+    .done(function(res) {
+      $form[0].reset();
+      $form.removeClass('sending');
+      alert("Thank you! We've gotten your message!");
+    })
+    .fail(function(res) {
+      alert(res.responseJSON.message);
+      $form.removeClass('sending');
+    });
 }
